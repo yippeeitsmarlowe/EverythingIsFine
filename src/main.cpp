641b8ab404f4dc8585ce27f4eebed1c2f446927b
MB2k05 <mbakerr2005@gmail.com>
2025-12-27 20:24:29 +0000

    Add files for opening credits and main menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Everything is fine!" << std::endl;
+int main()
+{
+    std::cout << "Everything is Fine! A game using SFML and C++20." << std::endl;
+    std::cout << "Made by Marlowe Baker and \"Stupid Games!\"" << std::endl;
+    std::cout << "You should see a window that opens as well as this writing to the console!" << std::endl;
     return 0;
 }
